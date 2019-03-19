abfd3c218a97b1259b99417f3cb3e3d42e51881a
ValerieHMoore <periwinkle17@outlook.com>
2019-03-19 14:59:12 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { Component } from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import { addItem } from  './actions/items';
-
+ 
 class App extends Component {
-
-  handleOnClick() {
-    this.props.store.dispatch(addItem());
+ 
+  handleOnClick = event => {
+    this.props.addItem()
   }
-
+ 
   render() {
     return (
       <div className="App">
-        <button onClick={(event) => this.handleOnClick(event)}>
+        <button onClick={this.handleOnClick}>
           Click
           </button>
         <p>{this.props.items.length}</p>
@@ -27,4 +27,7 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps, { addItem })(App);
+/* ES6 shorthand lets us pass in *one* value that will be read as the key and value */
+
+// export default connect(state => ({ items: state.items }), { addItem })(App);
